8c4ff86892c928b1c55dfe132e639d0936af794f
blockstarbdot <Brendan.hicks@stu.vertexacademies.org>
2024-10-18 14:13:26 -0400

    Add files via upload

---
+++ Brendan_Hicks_JS_04.js
@@ -0,0 +1,27 @@
+// Assign the variable 'favFruit' with a string name of your favorite fruit.
+
+let favFruit= "Brendan";
+
+
+// Declare a variable called 'price' and assign it a number that represents
+// the cost of 1 pound of your favorite fruit. (You can estimate.)
+
+let price = "0.50"
+
+// Declare a variable called 'pounds' and assign it a number that represents
+// a reasonable number of pounds of fruit to buy.
+
+let pounds = "160"
+
+// Log this information to the console in an appealing format.
+// Calculate the total cost and log it under the previous information.
+
+console.log("Favorite Fruit: Brendan")
+console.log("Price:" + price + "per pound")
+console.log("Weight:" + pounds + "lbs")
+console.log("Total Cost:", price*pounds, "USD")
+// Example:
+//            Favorite Fruit: Apple
+//            Price: $1.25 per pound
+//            Weight: 2.5lbs
+//            Total Cost: $3.125
